7b5da9b18fd2d28205120513d8f4ab7ec318016a
arashi <opp40519@gmail.com>
2022-03-31 14:38:42 +0800

    錯誤訊息確認

--- app.py
+++ app.py
@@ -221,7 +221,10 @@ def handle_message(event):
             reply.append(TextSendMessage(text="答案錯誤"))
             img_link = CheckAns(player_A, compare_ans, client_img)
             #reply.append(TextSendMessage(text=img_link))
-            reply.append(ImageSendMessage(original_content_url=img_link, preview_image_url=img_link))
+            try:
+                reply.append(ImageSendMessage(original_content_url=img_link, preview_image_url=img_link))
+            except:
+                reply.append(TextSendMessage(text="圖片訊息回傳失敗"))
             line_bot_api.reply_message(
                 event.reply_token,
                 reply
